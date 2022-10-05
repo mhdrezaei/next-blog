1c7c0de26429eeefa5b4f7e0521b6bbd63f452b9
mohammad rezaei <98229394+mhdrezaei@users.noreply.github.com>
2022-10-05 16:07:16 +0330

    add component for show featured post in hom page

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,25 @@
 import { Fragment } from "react";
 import Hero from "../components/home/hero";
-function HomePage(){
-
+import ListPost from "../components/posts/list-post";
+import { getData } from "../helper/util-post";
+function HomePage(props){
     return(
         <Fragment>
             <Hero/>
+            <ListPost posts={props.posts} headng='Featured Posts' />
         </Fragment>
     )
+}
+
+export async function getStaticProps(){
+    const fetchedData = getData();
+    const featuredPost = fetchedData.posts.filter(post => post.isFeatured);
+
+    return{
+        props:{
+            posts : featuredPost
+        }
+    }
 }
 
 
