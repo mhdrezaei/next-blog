730e1b30ee5cdb9d53f237c73f0ec7167f5043f6
mohammad rezaei <98229394+mhdrezaei@users.noreply.github.com>
2022-10-06 22:25:23 +0330

    create allpost page and singlepost page

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,4 +1,41 @@
-function SinglePostPage(){}
+import { getData } from "../../helper/util-post";
+import PostItem from "../../components/posts/post-item";
+import { Fragment } from "react";
+import { Breadcrumbs } from "@mui/material";
+import {useRouter} from "next/router"
+import PageBreadcrumbs from "../../components/layout/breadcrumb";
+import SinglePost from "../../components/posts/singlePost";
+
+
+function SinglePostPage(props){
+    
+    const router = useRouter();
+    const path = router.asPath;
+    return(
+        <Fragment>
+        <PageBreadcrumbs url={path}/>
+        <SinglePost post={props.post} />
+        {/* <PostItem post={props.post} /> */}
+        </Fragment>
+    )
+}
+
+
+
+export async function getServerSideProps(context){
+const {params} = context;
+const slug = params.slug;
+const fetchedData = getData();
+const thePost = fetchedData.posts.filter(post => post.slug === slug);
+
+
+return {
+    props:{
+        post : thePost
+    }
+}
+
+}
 
 
 export default SinglePostPage;
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,4 +1,33 @@
-function AllPostPage(){}
+import PageBreadcrumbs from "../../components/layout/breadcrumb";
+import {useRouter} from "next/router"
+import { Fragment } from "react";
+import ListPost from "../../components/posts/list-post";
+import { getData } from "../../helper/util-post";
 
+
+function AllPostPage(props){
+    const router = useRouter();
+    const path = router.pathname;
+    return(
+        <Fragment>
+        <PageBreadcrumbs url={path} />
+        <ListPost posts={props.posts} heading='All Posts' showBtn='' linkTo='' />
+        </Fragment>
+        
+
+    )
+}
+
+export async function getStaticProps(){
+    const fetchedData = getData();
+    const allPost = fetchedData.posts;
+    console.log(allPost)
+
+    return{
+        props:{
+            posts : allPost
+        }
+    }
+}
 
 export default AllPostPage;
