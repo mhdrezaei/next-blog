617e030bec67e777e15df029b75e1c019a98ad24
mohammad rezaei <98229394+mhdrezaei@users.noreply.github.com>
2022-10-17 16:42:23 +0330

    add Image componnent and code highlighter

--- components/posts/singlePost.js
+++ components/posts/singlePost.js
@@ -1,64 +1,85 @@
-import { Container, styled } from '@mui/system'
-import React from 'react'
-import Image from 'next/image'
-import theme from '../layout/theme'
-import IconLabelButtons from '../ui/button-showall'
-import HeaderPost from './header-post'
-import ReactMarkdown from "react-markdown"
-const Wrapper = styled('div')({
-    position : "relative",
-    backgroundColor : theme.palette.secondary.main,
-    borderRadius : 10,
-    marginTop : 50,
+import { Container, styled } from "@mui/system";
+import React from "react";
+import Image from "next/image";
+import theme from "../layout/theme";
+import IconLabelButtons from "../ui/button-showall";
+import HeaderPost from "./header-post";
+import ReactMarkdown from "react-markdown";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
+const Wrapper = styled("div")({
+  position: "relative",
+  backgroundColor: theme.palette.secondary.main,
+  borderRadius: 10,
+  marginTop: 50,
+  overflow: "hidden",
+  boxShadow: "0 0 8px #666",
+
+  "& .image": {
     overflow: "hidden",
-    boxShadow : "0 0 8px #666",
-
-    "& .image":{
-        overflow: "hidden",
-        filter : "blur(3px)"
+    filter: "blur(3px)",
+  },
+  "& .head-post": {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: 10,
+  },
+  "& .description": {
+    padding: 20,
+    "& a": {
+      textDecoration: "underline",
+      color: theme.palette.primary.main,
     },
-    "& .head-post":{
-        display : "flex",
-        justifyContent : "space-between",
-        alignItems : "center",
-        padding : 10,
-    },
-    "& .description" :{
-        padding : 20,
-        "& a":{
-            textDecoration : "underline",
-            color : theme.palette.primary.main
-        }
-    }
-
-})
+  },
+});
 function SinglePost(props) {
-    const {post} = props;
-    const { title, description , date , image , content } = post;
-    const src = `/images/posts/${image}`
-    console.log(content)
-    const newDate = new Date(date).toLocaleString('en-us' , {
-        year: 'numeric',
-        month: 'long',
-        day: 'numeric'
-    })
+  const { post } = props;
+  const { title, description, date, image, content } = post;
+  const path = `/images/posts/${image}`;
+  const newDate = new Date(date).toLocaleString("en-us", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+  const cutomRenderers = {
+    img:({node , ...props}) =>{
+      const {src , alt} = {...props};
+     return <Image src={`/images/posts/${src}`} alt={alt} width={600} height={300}/>
+      },
+      code:({node , ...props}) => {
+        const {className , children} = {...props};
+        const lang = className.split("-");
+        console.log(lang)
+        return <SyntaxHighlighter language={lang[1]}  children={children} style={atomDark} />
+      }
+    // image(image) {
+    //    return( <Image
+    //       src={`/images/posts/${image.src}`}
+    //       alt={image.alt}
+    //       width={600}
+    //       height={300}
+    //     />
+    //    )
+    // },
+  };
   return (
     <div>
-        <Container>
+      <Container>
         <Wrapper>
-        <HeaderPost title={title} image={src} date={newDate} />
-            <div className='image' >
-                <Image src={src} alt={title} width={1152} height={700} />
-            </div>      
-            <p className='description'>
-            <ReactMarkdown>{content}</ReactMarkdown>      
-            </p>
-        </Wrapper>  
+          <HeaderPost title={title} image={path} date={newDate} />
+          <div className="image">
+            <Image src={path} alt={title} width={1152} height={700} />
+          </div>
+          <div className="description">
+            <ReactMarkdown   components={cutomRenderers}>{content}</ReactMarkdown>
+          </div>
+        </Wrapper>
 
-        <IconLabelButtons path='/posts' />
-        </Container>
+        <IconLabelButtons path="/posts" />
+      </Container>
     </div>
-  )
+  );
 }
 
-export default SinglePost
+export default SinglePost;
