298d17396696d04197e212610dc380d3ec9e98c6
mohammad rezaei <98229394+mhdrezaei@users.noreply.github.com>
2022-10-18 15:46:49 +0330

    add contact component to contact page

--- pages/contact.js
+++ pages/contact.js
@@ -1,4 +1,16 @@
-function ContactPage(){}
-
+import { Fragment } from "react";
+import ContactForm from "../components/contact/contact-form";
+import PageBreadcrumbs from "../components/layout/breadcrumb";
+import { useRouter } from "next/router"
+function ContactPage() {
+    const router = useRouter();
+    const path = router.asPath;
+  return (
+    <Fragment>
+      <PageBreadcrumbs url={path} />
+      <ContactForm />
+    </Fragment>
+  );
+}
 
 export default ContactPage;
