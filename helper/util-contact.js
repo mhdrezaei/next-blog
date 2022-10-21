25edca62ea883ed26adbb4a1254d374158cf6a15
mohammad rezaei <98229394+mhdrezaei@users.noreply.github.com>
2022-10-21 03:31:16 +0330

    add some function to save contact messages

--- helper/util-contact.js
+++ helper/util-contact.js
@@ -2,9 +2,12 @@ import path from "path";
 import fs from "fs";
 import { createPath } from "./util-post";
 
-
-export function saveMessage(data){
-    const filepath = createPath('message.json');
-    const fileData = fs.readFileSync(filepath);
-
+export function saveMessage(contactData) {
+  const filepath = createPath("message.json");
+  console.log(filepath);
+  const fileData = fs.readFileSync(filepath);
+  const jsonData = JSON.parse(fileData);
+  jsonData.push(contactData);
+  fs.writeFileSync(filepath, JSON.stringify(jsonData));
+  return { message: "success" };
 }
