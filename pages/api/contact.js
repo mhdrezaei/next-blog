e75f1779187d1e728e48c305b275a14f411c9357
mohammad rezaei <98229394+mhdrezaei@users.noreply.github.com>
2022-10-21 03:32:21 +0330

    manage request for save messages

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,3 +1,32 @@
-export default function contactHandler(req , res){
-    console.log('hi')
+import { saveMessage } from "../../helper/util-contact";
+
+export default function contactHandler(req, res) {
+  if (req.method === "POST") {
+    const { email, name, message } = req.body;
+
+    if (
+      !email ||
+      !email.includes("@") ||
+      !name ||
+      name.trim() === "" ||
+      !message ||
+      message.trim() === ""
+    ) {
+      res.status(422).json({ message: "Invalid input." });
+      return;
+    }
+
+    const newContactDetail = {
+      id: new Date().toISOString(),
+      name,
+      email,
+      message,
+    };
+    const sendMessage = saveMessage(newContactDetail);
+    if (sendMessage.message === "success") {
+      res.status(200).json({ message: "your message successfully sent!" });
+    } else {
+      res.status(500).json({ message: "something went wrong!" });
+    }
+  }
 }
