31fb4b576ef31e6d7b24dd8ea4c5ea84e81c6727
mohammad rezaei <98229394+mhdrezaei@users.noreply.github.com>
2022-10-28 00:22:14 +0330

    add one functon to read from feedbacks.json and send to [slug].js page

--- helper/util-contact.js
+++ helper/util-contact.js
@@ -11,3 +11,20 @@ export function saveMessage(contactData) {
   fs.writeFileSync(filepath, JSON.stringify(jsonData));
   return { message: "success" };
 }
+
+export function saveFeedback(feedbackData) {
+  const filepath = createPath("feedback.json");
+  const fileData = fs.readFileSync(filepath);
+  const jsonData = JSON.parse(fileData);
+  jsonData.push(feedbackData);
+  fs.writeFileSync(filepath, JSON.stringify(jsonData));
+  return { message: "success" };
+}
+
+export function showFeedback(slug){
+  const filepath = createPath("feedback.json");
+  const fileData = fs.readFileSync(filepath);
+  const feedbacks = JSON.parse(fileData);
+  const filterFeedback = feedbacks.filter(feedback => feedback.slug === slug )
+  return {feedbacks : filterFeedback }
+}
