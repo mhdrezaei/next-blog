dc683b4ba5933639d9675a00fc27def951d4c8f2
رضائی - محمد <rezaee-moha@tehran.iri>
2022-10-28 11:50:12 +0330

    add some style

--- components/comments/CommentItem.js
+++ components/comments/CommentItem.js
@@ -1,24 +1,34 @@
-import styled from '@emotion/styled'
-import React from 'react'
+import styled from "@emotion/styled";
+import React from "react";
+import theme from "../layout/theme";
 
-const CommentBody = styled('div')({
-
-})
-
+const CommentBody = styled("div")({
+  backgroundColor: theme.palette.primary.light,
+  padding: 15,
+  margin: "5px",
+  borderRadius: "5px",
+  "& .heading": {
+    fontSize: 22,
+    color: theme.palette.secondary.main,
+    textAlign: "left",
+  },
+  "& .name": {
+    fontSize: 20,
+    fontFamily: "cursive",
+    color: theme.palette.secondary.main,
+    textAlign: "right",
+  },
+});
 
 function CommentItem(props) {
-    const { name , message } = props;
-    console.log(props)
+  const { name, message } = props;
   return (
-
-    <div>
-    <h1>{message}</h1>
-    <br/>
-    <p>{name}</p>
-    </div>
-
-
-    )
+    <CommentBody>
+      <p className="heading">{message}</p>
+      <br />
+      <p className="name">{name}</p>
+    </CommentBody>
+  );
 }
 
-export default CommentItem
+export default CommentItem;
